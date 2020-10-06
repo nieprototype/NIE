50bc792d955f6058dd2e365d74792776dffbadf3
fasliya <fazy.hfb@gmail.com>
2020-10-06 22:57:47 +0530

    Implementing Gurugedara TV Program Section

---
+++ gurugedara.php
@@ -0,0 +1,123 @@
+<?php include "includes/header.html"; ?>
+
+
+<section style="margin-top: 3%;">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div style="text-align: left; margin-left: 0%;" class="hero__text">
+
+                    <h2 style="font-size: 50px; color: #0f0f16;"><span style="font-family: 'fn_nilanthi', Arial, sans-serif; font-size: 70px;">.=ref.or</span> TV CHANNEL PROGRAM GUIDE</h2>
+
+                    <h5>Home > Gurugedara TV Channel</h5>
+
+
+                </div>
+            </div>
+
+
+        </div>
+
+
+    </div>
+    </div>
+    </div>
+</section>
+
+
+<!-- Content Begin -->
+
+<div class="kt-container  kt-container--fluid  kt-grid__item kt-grid__item--fluid">
+    <div class="container">
+
+
+        <div class="row">
+            <div class="col-12">
+
+                <form style="position:relative;
+    -webkit-box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;
+       -moz-box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;
+            box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;" action="#" class="filter__form">
+                    <div class="filter__form__item filter__form__item--search col-5">
+
+
+                        <div class="kt-widget14__header">
+                            <h3 class="kt-widget14__title">
+                                Watch Live Gurugedara TV Program
+                            </h3>
+                            <span class="kt-widget14__desc">
+
+
+
+
+
+                       									</span>
+                        </div>
+
+
+                        <div class="filter__form__input">
+                            <br>
+                            <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/XyqOR3Ou1SY"
+                                    frameborder="0"
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                    allowfullscreen></iframe>
+                        </div>
+                    </div>
+                    <div style="margin-left: 10%;" class="filter__form__item col-5">
+                        <p style="text-align: right;"><a href="#" class=""><img width="15%" src="img/tv/live.png"></a></p>
+
+
+                        <div class="kt-widget14__header kt-margin-b-30">
+                            <h2 style="font-size: 28px; color: #E9AD28;">Now Playing </h2> <h3 style="font-weight: 400; margin-bottom: -4%;" class="kt-widget14__title">
+                                2020-10-06 | Geography | A/L
+                            </h3>
+
+                        </div>
+
+
+                        <h5 style="text-align: left;">
+
+                          Followings are the provided materials for the current session. Just click on them to download.
+
+                        </h5>
+                        <br>
+
+                        <div style="text-align: left;">
+                        <img src="img/downloads/pdf2.png" width="20%">
+
+                            <h5 style="text-align: left; margin-left: 20%; margin-top: -12.8%;">
+                                Geograpy_Lesson_8.pdf
+                            </h5>
+
+                        </div>
+
+                        <br><br>
+
+                        <div style="text-align: left">
+                            <img style="margin-left: 2.5%;" src="img/downloads/down.png" width="16%">
+
+                            <h5 style="text-align: left; margin-left: 20%; margin-top: -11%;">
+                                Geograpy_Pastpaper_Questions_8.pdf
+                            </h5>
+
+                        </div>
+
+
+                    </div>
+
+                </form>
+
+
+                <br><br>
+            </div>
+
+
+        </div>
+
+
+    </div>
+</div>
+
+<!-- Content End -->
+
+<?php include "includes/footer.html"; ?>
