d13ea36dcf0f00b5c2a9cb62814e52df64112079
ulidu <ulidutheerake@gmail.com>
2020-10-10 12:06:31 +0530

    Implementing Login

---
+++ forms.php
@@ -0,0 +1,16 @@
+<?php include "includes/header.html"; ?>
+
+    <div class="kt-container  kt-container--fluid  kt-grid__item kt-grid__item--fluid">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+
+
+
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+<?php include "includes/footer.html"; ?>
