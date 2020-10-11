c30879085ac27dbcdb73bc3a99a7a57aeacc2498
NipunEranda <namarasekara71@gmail.com>
2020-10-11 12:20:20 +0530

    Added background image to research page

--- researches.php
+++ researches.php
@@ -4,76 +4,78 @@
 <script src="js/researches.js"></script>
 
 
-<div class="container-fluid">
-    <div class="topic">
-        <h1>NIE Educational Software</h1>
-    </div>
-    <div class="row">
-        <div class="col">
-            <div class="rjcontainer">
-                <div class="topics">
-                    <h2>Researches</h2>
-                </div>
-                <div class="row">
-                    <div class="col-sm">
-                        <img src="img/SoftwarePackages/pack.png" alt="Software1"><br />
-                        <div class="overlay">
-                            <div class="text">Education Software Package For Grade 6 Students<br /><br /><a href="">more</a></div>
+<div class="container-fluid" style="background-image: url('../NIE/img/researches/geometric-2880x1800-shapes-dark-background-black-yellow-gradient-hd-4k-14372.png'); background-size: cover; padding: 20px;">
+    <div style="background-color: rgba(0, 0, 0, 0.7);">
+        <div class="topic">
+            <h1>RESEARCHES AND JOURNALS</h1>
+        </div>
+        <div class="row" style="padding-bottom: 2%;">
+            <div class="col">
+                <div class="rjcontainer">
+                    <div class="topics">
+                        <h2>Researches</h2>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm">
+                            <img src="img/SoftwarePackages/pack.png" alt="Software1"><br />
+                            <div class="overlay">
+                                <div class="text">Education Software Package For Grade 6 Students<br /><br /><a href="">more</a></div>
+                            </div>
+                        </div>
+                        <div class="col-sm">
+                            <img src="img/SoftwarePackages/pack.png" alt="Software1"><br />
+                            <div class="overlay">
+                                <div class="text">Education Software Package For Grade 6 Students<br /><br /><a href="">more</a></div>
+                            </div>
                         </div>
                     </div>
-                    <div class="col-sm">
-                        <img src="img/SoftwarePackages/pack.png" alt="Software1"><br />
-                        <div class="overlay">
-                            <div class="text">Education Software Package For Grade 6 Students<br /><br /><a href="">more</a></div>
+                    <div class="row">
+                        <div class="col-sm">
+                            <img src="img/SoftwarePackages/pack.png" alt="Software1"><br />
+                            <div class="overlay">
+                                <div class="text">Education Software Package For Grade 6 Students<br /><br /><a href="">more</a></div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-sm">
-                        <img src="img/SoftwarePackages/pack.png" alt="Software1"><br />
-                        <div class="overlay">
-                            <div class="text">Education Software Package For Grade 6 Students<br /><br /><a href="">more</a></div>
-                        </div>
-                    </div>
-                    <div class="col-sm">
-                        <img src="img/SoftwarePackages/pack.png" alt="Software1"><br />
-                        <div class="overlay">
-                            <div class="text">Education Software Package For Grade 6 Students<br /><br /><a href="">more</a></div>
+                        <div class="col-sm">
+                            <img src="img/SoftwarePackages/pack.png" alt="Software1"><br />
+                            <div class="overlay">
+                                <div class="text">Education Software Package For Grade 6 Students<br /><br /><a href="">more</a></div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="col">
-            <div class="rjcontainer">
-                <div class="topics">
-                    <h2>Journals</h2>
-                </div>
-                <div class="row">
-                    <div class="col-sm">
-                        <img src="img/SoftwarePackages/pack.png" alt="Software1"><br />
-                        <div class="overlay">
-                            <div class="text">Education Software Package For Grade 6 Students<br /><br /><a href="">more</a></div>
+            <div class="col" style="border-left: lightgray solid 1px;">
+                <div class="rjcontainer">
+                    <div class="topics">
+                        <h2>Journals</h2>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm">
+                            <img src="img/SoftwarePackages/pack.png" alt="Software1"><br />
+                            <div class="overlay">
+                                <div class="text">Education Software Package For Grade 6 Students<br /><br /><a href="">more</a></div>
+                            </div>
+                        </div>
+                        <div class="col-sm">
+                            <img src="img/SoftwarePackages/pack.png" alt="Software1"><br />
+                            <div class="overlay">
+                                <div class="text">Education Software Package For Grade 6 Students<br /><br /><a href="">more</a></div>
+                            </div>
                         </div>
                     </div>
-                    <div class="col-sm">
-                        <img src="img/SoftwarePackages/pack.png" alt="Software1"><br />
-                        <div class="overlay">
-                            <div class="text">Education Software Package For Grade 6 Students<br /><br /><a href="">more</a></div>
+                    <div class="row">
+                        <div class="col-sm">
+                            <img src="img/SoftwarePackages/pack.png" alt="Software1"><br />
+                            <div class="overlay">
+                                <div class="text">Education Software Package For Grade 6 Students<br /><br /><a href="">more</a></div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-sm">
-                        <img src="img/SoftwarePackages/pack.png" alt="Software1"><br />
-                        <div class="overlay">
-                            <div class="text">Education Software Package For Grade 6 Students<br /><br /><a href="">more</a></div>
-                        </div>
-                    </div>
-                    <div class="col-sm">
-                        <img src="img/SoftwarePackages/pack.png" alt="Software1"><br />
-                        <div class="overlay">
-                            <div class="text">Education Software Package For Grade 6 Students<br /><br /><a href="">more</a></div>
+                        <div class="col-sm">
+                            <img src="img/SoftwarePackages/pack.png" alt="Software1"><br />
+                            <div class="overlay">
+                                <div class="text">Education Software Package For Grade 6 Students<br /><br /><a href="">more</a></div>
+                            </div>
                         </div>
                     </div>
                 </div>
