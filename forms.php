942b5d322f8b378c338df3b3aa1ca1838012bfaf
ulidu <ulidutheerake@gmail.com>
2020-10-13 19:03:42 +0530

    Implementing Login

--- forms.php
+++ forms.php
@@ -80,7 +80,8 @@
                                 <div class="row">
 
 
-                                    <div style="cursor: pointer;" onclick="location.href='down_manage.php';" class="col-5">
+                                    <div style="cursor: pointer;" onclick="location.href='down_manage.php';"
+                                         class="col-5">
                                         <div style="position:relative;
     -webkit-box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;
        -moz-box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;
@@ -89,22 +90,33 @@
                                             <div class="kt-portlet__body">
                                                 <div class="kt-iconbox__body">
                                                     <div class="kt-iconbox__icon">
-                                                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1" class="kt-svg-icon">
-                                                            <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                                        <svg xmlns="http://www.w3.org/2000/svg"
+                                                             xmlns:xlink="http://www.w3.org/1999/xlink" width="24px"
+                                                             height="24px" viewBox="0 0 24 24" version="1.1"
+                                                             class="kt-svg-icon">
+                                                            <g stroke="none" stroke-width="1" fill="none"
+                                                               fill-rule="evenodd">
                                                                 <rect x="0" y="0" width="24" height="24"/>
-                                                                <path d="M2,13 C2,12.5 2.5,12 3,12 C3.5,12 4,12.5 4,13 C4,13.3333333 4,15 4,18 C4,19.1045695 4.8954305,20 6,20 L18,20 C19.1045695,20 20,19.1045695 20,18 L20,13 C20,12.4477153 20.4477153,12 21,12 C21.5522847,12 22,12.4477153 22,13 L22,18 C22,20.209139 20.209139,22 18,22 L6,22 C3.790861,22 2,20.209139 2,18 C2,15 2,13.3333333 2,13 Z" fill="#000000" fill-rule="nonzero" opacity="0.3"/>
-                                                                <rect fill="#000000" opacity="0.3" transform="translate(12.000000, 8.000000) rotate(-180.000000) translate(-12.000000, -8.000000) " x="11" y="1" width="2" height="14" rx="1"/>
-                                                                <path d="M7.70710678,15.7071068 C7.31658249,16.0976311 6.68341751,16.0976311 6.29289322,15.7071068 C5.90236893,15.3165825 5.90236893,14.6834175 6.29289322,14.2928932 L11.2928932,9.29289322 C11.6689749,8.91681153 12.2736364,8.90091039 12.6689647,9.25670585 L17.6689647,13.7567059 C18.0794748,14.1261649 18.1127532,14.7584547 17.7432941,15.1689647 C17.3738351,15.5794748 16.7415453,15.6127532 16.3310353,15.2432941 L12.0362375,11.3779761 L7.70710678,15.7071068 Z" fill="#000000" fill-rule="nonzero" transform="translate(12.000004, 12.499999) rotate(-180.000000) translate(-12.000004, -12.499999) "/>
+                                                                <path d="M2,13 C2,12.5 2.5,12 3,12 C3.5,12 4,12.5 4,13 C4,13.3333333 4,15 4,18 C4,19.1045695 4.8954305,20 6,20 L18,20 C19.1045695,20 20,19.1045695 20,18 L20,13 C20,12.4477153 20.4477153,12 21,12 C21.5522847,12 22,12.4477153 22,13 L22,18 C22,20.209139 20.209139,22 18,22 L6,22 C3.790861,22 2,20.209139 2,18 C2,15 2,13.3333333 2,13 Z"
+                                                                      fill="#000000" fill-rule="nonzero" opacity="0.3"/>
+                                                                <rect fill="#000000" opacity="0.3"
+                                                                      transform="translate(12.000000, 8.000000) rotate(-180.000000) translate(-12.000000, -8.000000) "
+                                                                      x="11" y="1" width="2" height="14" rx="1"/>
+                                                                <path d="M7.70710678,15.7071068 C7.31658249,16.0976311 6.68341751,16.0976311 6.29289322,15.7071068 C5.90236893,15.3165825 5.90236893,14.6834175 6.29289322,14.2928932 L11.2928932,9.29289322 C11.6689749,8.91681153 12.2736364,8.90091039 12.6689647,9.25670585 L17.6689647,13.7567059 C18.0794748,14.1261649 18.1127532,14.7584547 17.7432941,15.1689647 C17.3738351,15.5794748 16.7415453,15.6127532 16.3310353,15.2432941 L12.0362375,11.3779761 L7.70710678,15.7071068 Z"
+                                                                      fill="#000000" fill-rule="nonzero"
+                                                                      transform="translate(12.000004, 12.499999) rotate(-180.000000) translate(-12.000004, -12.499999) "/>
                                                             </g>
                                                         </svg>
                                                     </div>
                                                     <div class="kt-iconbox__desc">
                                                         <h3 class="kt-iconbox__title">
-                                                            <a class="kt-link" href="down_manage.php">Manage Downloads</a>
+                                                            <a class="kt-link" href="down_manage.php">Manage
+                                                                Downloads</a>
                                                         </h3>
                                                         <div class="kt-iconbox__content">
                                                             <a style="color: #9392a0" class="kt-link"
-                                                               href="down_manage.php">Manage all Downloadable Materials</a>
+                                                               href="down_manage.php">Manage all Downloadable
+                                                                Materials</a>
                                                         </div>
                                                     </div>
                                                 </div>
@@ -112,7 +124,8 @@
                                         </div>
                                     </div>
 
-                                    <div style="cursor: pointer;" onclick="location.href='down_manage.php';" class="col-5">
+                                    <div style="cursor: pointer;" onclick="location.href='down_manage.php';"
+                                         class="col-5">
                                         <div style="position:relative;
     -webkit-box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;
        -moz-box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;
@@ -121,34 +134,42 @@
                                             <div class="kt-portlet__body">
                                                 <div class="kt-iconbox__body">
                                                     <div class="kt-iconbox__icon">
-                                                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1" class="kt-svg-icon">
-                                                            <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                                        <svg xmlns="http://www.w3.org/2000/svg"
+                                                             xmlns:xlink="http://www.w3.org/1999/xlink" width="24px"
+                                                             height="24px" viewBox="0 0 24 24" version="1.1"
+                                                             class="kt-svg-icon">
+                                                            <g stroke="none" stroke-width="1" fill="none"
+                                                               fill-rule="evenodd">
                                                                 <rect x="0" y="0" width="24" height="24"/>
-                                                                <path d="M3,5 L21,5 C21.5522847,5 22,5.44771525 22,6 L22,17 C22,17.5522847 21.5522847,18 21,18 L3,18 C2.44771525,18 2,17.5522847 2,17 L2,6 C2,5.44771525 2.44771525,5 3,5 Z M9.632,10.066 L11.032,10.066 L11.032,9.044 L7.035,9.044 L7.035,10.066 L8.435,10.066 L8.435,14 L9.632,14 L9.632,10.066 Z M14.935,14 L16.846,9.044 L15.523,9.044 L14.382,12.558 L14.354,12.558 L13.206,9.044 L11.862,9.044 L13.738,14 L14.935,14 Z" fill="#000000"/>
-                                                                <rect fill="#000000" opacity="0.3" x="3" y="19" width="18" height="1" rx="0.5"/>
+                                                                <path d="M3,5 L21,5 C21.5522847,5 22,5.44771525 22,6 L22,17 C22,17.5522847 21.5522847,18 21,18 L3,18 C2.44771525,18 2,17.5522847 2,17 L2,6 C2,5.44771525 2.44771525,5 3,5 Z M9.632,10.066 L11.032,10.066 L11.032,9.044 L7.035,9.044 L7.035,10.066 L8.435,10.066 L8.435,14 L9.632,14 L9.632,10.066 Z M14.935,14 L16.846,9.044 L15.523,9.044 L14.382,12.558 L14.354,12.558 L13.206,9.044 L11.862,9.044 L13.738,14 L14.935,14 Z"
+                                                                      fill="#000000"/>
+                                                                <rect fill="#000000" opacity="0.3" x="3" y="19"
+                                                                      width="18" height="1" rx="0.5"/>
                                                             </g>
                                                         </svg>
                                                     </div>
                                                     <div class="kt-iconbox__desc">
                                                         <h3 class="kt-iconbox__title">
-                                                            <a class="kt-link" href="guru_manage.php">Manage Gurugedara Materials</a>
+                                                            <a class="kt-link" href="guru_manage.php">Manage Gurugedara
+                                                                Materials</a>
                                                         </h3>
                                                         <div class="kt-iconbox__content">
-                                                            <a style="color: #9392a0" class="kt-link" href="task_add.php">Manage all Supportive Materials for Gurugedara</a>
+                                                            <a style="color: #9392a0" class="kt-link"
+                                                               href="task_add.php">Manage all Supportive Materials for
+                                                                Gurugedara</a>
                                                         </div>
                                                     </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
-
 
 
                                 </div>
 
 
                             </div>
-                         </div>
+                        </div>
                     </div>
                 </div>
 
@@ -159,13 +180,10 @@
     </div>
 
 
-
-        </section>
-
-
-        <br><br><br>
+    </section>
 
 
+    <br><br><br>
 
 
 <?php include "includes/footer.html"; ?>
