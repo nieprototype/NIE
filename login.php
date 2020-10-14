b73dde17bc403cfbc90d22256aea71b60d6d603e
fasliya <fazy.hfb@gmail.com>
2020-10-14 12:12:09 +0530

    Implementing Login

--- login.php
+++ login.php
@@ -9,21 +9,24 @@
                     <div style="background-color: #E9AD28;" class="veen">
                         <div class="login-btn splits">
 
-                            <h3 style="color: #fff; font-size: 26px; font-family: 'Barlow Condensed', sans-serif;">Already an user ?</h3>
+                            <h3 style="color: #fff; font-size: 26px; font-family: 'Barlow Condensed', sans-serif;">
+                                Already an user ?</h3>
                             <br>
                             <button class="active">Login</button>
                         </div>
                         <div class="rgstr-btn splits">
-                            <h3 style="color: #fff; font-size:26px; font-family: 'Barlow Condensed', sans-serif;">Don't have an account ?</h3>
+                            <h3 style="color: #fff; font-size:26px; font-family: 'Barlow Condensed', sans-serif;">Don't
+                                have an account ?</h3>
                             <br>
                             <button style="font-family: 'Barlow Condensed', sans-serif">Register</button>
                         </div>
                         <div style="background-color: #1a1a1a;" class="wrapper">
-                            <form id="hid1"  autocomplete="off" style="background-color: transparent;">
+                            <form id="hid1" autocomplete="off" style="background-color: transparent;">
                                 <h2 style="color: #fff; font-family: 'Barlow Condensed', sans-serif;">NIE STAFF</h2>
-                                
+
                                 <img width="70%" src="img/downloads/login.png">
-                                <h4 style="color: #fff; margin-bottom: 15px; margin-top: 10px; font-family: 'Barlow Condensed', sans-serif;">Login</h4>
+                                <h4 style="color: #fff; margin-bottom: 15px; margin-top: 10px; font-family: 'Barlow Condensed', sans-serif;">
+                                    Login</h4>
                                 <div class="mail">
                                     <input id="loginmail" readonly type="text" onfocus="if (this.hasAttribute('readonly')) { this.removeAttribute('readonly');
     // fix for mobile safari to show virtual keyboard
@@ -39,16 +42,18 @@
                                 </div>
                             </form>
 
-                            <form id="hid2" autocomplete="off" style="background-color: transparent; display: none;" action="" method="POST">
+                            <form id="hid2" autocomplete="off" style="background-color: transparent; display: none;"
+                                  action="" method="POST">
                                 <h2 style="color: #fff; font-family: 'Barlow Condensed', sans-serif;">NIE STAFF</h2>
 
                                 <img width="70%" src="img/downloads/login.png">
-                                <h4 style="color: #fff; margin-bottom: 15px; margin-top: 10px; font-family: 'Barlow Condensed', sans-serif;">Register</h4>
+                                <h4 style="color: #fff; margin-bottom: 15px; margin-top: 10px; font-family: 'Barlow Condensed', sans-serif;">
+                                    Register</h4>
 
                                 <h5 style="color: #fff; font-weight: 400;">
                                     Click the Register button below to register as an a teacher
                                 </h5>
-<br>
+                                <br>
                                 <div class="submit">
                                     <button type="submit" class="dark">Register</button>
                                 </div>
